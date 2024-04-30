5649fdf4f25a47268fd8dd9ae9ec365c2f42fb03
Hem Bahadur Bohara <153645039+hembahadurbohara@users.noreply.github.com>
2024-04-30 19:37:35 +0545

    Update Messege.jsx

--- src/componants/Messege.jsx
+++ src/componants/Messege.jsx
@@ -7,7 +7,7 @@ function Messege() {
         <div class="text-center msg pb-2">Message From CEO</div>
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 col-12 text-center pt-4">
-                <div class="pic pt-5 mp"><img class='rounded-4' src="https://scontent.fktm3-1.fna.fbcdn.net/v/t1.6435-9/88194215_137588141093899_5264478527243157504_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3a23d8&_nc_ohc=sVjdnT8A_sYAX-I_j87&_nc_ht=scontent.fktm3-1.fna&oh=00_AfDe6MdpmFtPKoA2m2hE8S2jT4R-Beeu4le4c15QSQ1SFQ&oe=660BEAAF" alt="" /></div>
+                <div class="pic pt-5 mp"><img class='rounded-4' src="https://scontent.fdhi1-1.fna.fbcdn.net/v/t1.6435-9/88194215_137588141093899_5264478527243157504_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ytq7FS4MOocAb5L0Pab&_nc_ht=scontent.fdhi1-1.fna&oh=00_AfALECA1x7GMRflxKsfDGznMbN2eqMq-ZVWYQE1mwmTJlw&oe=665861AF" alt="" /></div>
                 <div class="n pt-2">Hem Bahadur Bohara</div>
                 <div class="n">CEO</div>
             </div>
